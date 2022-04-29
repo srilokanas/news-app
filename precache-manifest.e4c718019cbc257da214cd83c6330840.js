cde608e49a4e2c89fd66f219965dba6ab1cbbaa7
srilokanas <srilokanas@gmail.com>
2022-04-29 09:46:44 +0530

    Updates

---
+++ precache-manifest.e4c718019cbc257da214cd83c6330840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa905349df7e675d85ac4410d6278dfc",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "51d17a43fff16bc3ae78",
+    "url": "/news-app/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/news-app/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-app/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d17a43fff16bc3ae78",
+    "url": "/news-app/static/js/main.514d23b5.chunk.js"
+  },
+  {
+    "revision": "b64b9c0fbe7d9c76b462",
+    "url": "/news-app/static/js/runtime-main.ca80c89a.js"
+  }
+]);
